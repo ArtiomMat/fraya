3c2ae82b10444a04828fabb854340cec8a03485a
Your Name <you@example.com>
2026-04-21 13:19:44 +0300

    Make the eye controller a little more robust

--- src/main.rs
+++ src/main.rs
@@ -21,9 +21,12 @@ struct EyeController {
     right: f32,
     backward: f32,
     left: f32,
+    upward: f32,
+    downward: f32,
     look_x: f32,
     look_y: f32,
     is_looking: bool,
+    reset_look_z: bool,
 }
 
 impl EyeController {
@@ -34,8 +37,11 @@ impl EyeController {
             right: 0.0,
             backward: 0.0,
             left: 0.0,
+            upward: 0.0,
+            downward: 0.0,
             look_x: 0.0,
             look_y: 0.0,
+            reset_look_z: false,
             is_looking: false,
         }
     }
@@ -54,6 +60,9 @@ impl EyeController {
             Key::D => Self::apply_move(&mut self.right, down),
             Key::W => Self::apply_move(&mut self.forward, down),
             Key::S => Self::apply_move(&mut self.backward, down),
+            Key::E => Self::apply_move(&mut self.upward, down),
+            Key::Q => Self::apply_move(&mut self.downward, down),
+            Key::Z => self.reset_look_z = true,
             _ => {}
         }
     }
@@ -61,8 +70,6 @@ impl EyeController {
     pub fn process_mouse_button(&mut self, b: MouseButton, down: bool) {
         match b {
             MouseButton::Left => {
-                self.look_x = 0.0;
-                self.look_y = 0.0;
                 if down {
                     self.is_looking = true;
                 } else {
@@ -80,17 +87,23 @@ impl EyeController {
         }
     }
 
-    pub fn control_eye(&self, eye: &mut Eye) {
+    pub fn control_eye(&mut self, eye: &mut Eye) {
         let position_delta = eye.rotation
             * Vec3::new(
                 vec3::RIGHT.x * (self.right - self.left),
-                0.0,
+                vec3::UP.y * (self.upward - self.downward),
                 vec3::FORWARD.z * (self.forward - self.backward),
             )
             .normalize_or_zero();
 
         eye.position += self.speed * position_delta;
         eye.rotation *= Quat::from_euler(glam::EulerRot::XYZ, self.look_x, self.look_y, 0.0);
+        self.look_x = 0.0;
+        self.look_y = 0.0;
+        if self.reset_look_z {
+            self.reset_look_z = false;
+            eye.rotation *= Quat::from_euler(glam::EulerRot::XYZ, 0.0, 0.0, eye.rotation.to_euler(glam::EulerRot::XYZ).2).inverse();
+        }
     }
 }
 
--- src/math/vec3.rs
+++ src/math/vec3.rs
@@ -1,7 +1,8 @@
 /// A type alias for `glam` but leaves the ability to change it later.
 pub type Vec3 = glam::Vec3;
 
-pub const UP: Vec3 = Vec3::new(0.0, 1.0, 0.0);
+// TODO: Not conventional(1 is)
+pub const UP: Vec3 = Vec3::new(0.0, -1.0, 0.0);
 pub const FORWARD: Vec3 = Vec3::new(0.0, 0.0, -1.0);
 pub const RIGHT: Vec3 = Vec3::new(1.0, 0.0, 0.0);
 
